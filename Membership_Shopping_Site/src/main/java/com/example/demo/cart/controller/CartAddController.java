67f75df0d2094af35548ebf8a2e5b3a235d6f68b
加藤怜 <jatgadm@gmail.com>
2019-12-14 23:33:04 +0900

    cart機能実装

--- Membership_Shopping_Site/src/main/java/com/example/demo/cart/controller/CartAddController.java
+++ Membership_Shopping_Site/src/main/java/com/example/demo/cart/controller/CartAddController.java
@@ -1,11 +1,19 @@
 package com.example.demo.cart.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.cart.model.Item;
+import com.example.demo.product.domain.Product;
 
 @Controller
 public class CartAddController {
@@ -15,30 +23,30 @@ public class CartAddController {
 	HttpServletRequest request;
 
 	@GetMapping("/cartAdd")
-	public String getCartAdd() {
-		//int id = Integer.parseInt(request.getParameter("id"));
-		//System.out.println(id);
-		/*List<Item> cart = (List<Item>) session.getAttribute("cart");
+	public String getCartAdd(@RequestParam String id,Model model) {
+		int id1 = Integer.parseInt(id);
+
+		List<Item> cart = (List<Item>) session.getAttribute("cart");
 		if(cart == null) {
 			cart = new ArrayList<Item>();
-
 			session.setAttribute("cart", cart);
 		}
 		for(Item i : cart) {
-			if(i.getProduct().getId()==id) {
+			if(i.getProduct().getId()==id1) {
 				i.setCount(i.getCount()+1);
 				return "cart/cart";
 			}
 		}
-		List<Product> list = (List<Product>) session.getAttribute("list");
-		for(Product p :list) {
-			if(p.getId() == id) {
+		List<Product> productList = (List<Product>) session.getAttribute("productList");
+		for(Product p :productList) {
+			if(p.getId() == id1) {
 				Item i = new Item();
 				i.setProduct(p);
 				i.setCount(1);
 				cart.add(i);
 			}
-		}*/
+		}
+
 		return "cart/cart";
 
 	}
--- Membership_Shopping_Site/src/main/java/com/example/demo/cart/controller/CartRemoveController.java
+++ Membership_Shopping_Site/src/main/java/com/example/demo/cart/controller/CartRemoveController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,11 +17,12 @@ public class CartRemoveController {
 	@Autowired
 	HttpSession session;
 	@SuppressWarnings("unchecked")
-	@GetMapping("/catRemove")
-	public String getRemove(@RequestParam("id") int id ) {
+	@GetMapping("/cartRemove")
+	public String getRemove(@RequestParam String id,Model model ) {
+		int id1 = Integer.parseInt(id);
 		List<Item> cart = (List<Item>) session.getAttribute("cart");
 		for(Item i : cart) {
-			if(i.getProduct().getId()==id) {
+			if(i.getProduct().getId()==id1) {
 				cart.remove(i);
 				break;
 			}
